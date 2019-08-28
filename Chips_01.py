4512c18eb4758dc9477043f3d928e1165aab0f67
optimid <48894093+optimid@users.noreply.github.com>
2019-08-28 12:21:12 +0100

    Update Chips_01.py

--- Chips_01.py
+++ Chips_01.py
@@ -14,6 +14,7 @@ except ImportError as err:
 #================================================================
 class Chip(object):
 
+
     def __init__(self, x, y, height, width, screen, image):
         self.image = pygame.image.load(image)
         self.x = x
@@ -21,22 +22,30 @@ class Chip(object):
         self.width = width
         self.height = height
         self.screen = screen
-        self.rect = self.image.get_rect()
         self.times = 1
+        self.rect = self.image.get_rect()
 
     #acc the accration due to gravity
     #floor is where it will stop
     def gravity(self, acc, floor):
-
         if self.y < floor - (self.height) + (self.height / 10):
             self.y = self.y + acc * self.times
             self.times = self.times + 1
 
         else:
             self.y = floor - (self.height) + (self.height / 10)
-    #collision detection
+
+    #Collision dectection
     def collision(self, other):
-        return self.rect.colliderect(other.rect)
+        my_size = (self.y - (self.height / 2), self.y + (self.height / 2))
+        other_size = (other.y - (other.height / 2), other.y + (other.height / 2))
+        #print('my size   :{}\nother size:{}'.format(my_size, other_size))
+        if (my_size[0] <= other_size[0] and other_size[0] <= my_size[1]) or (my_size[0] <= other_size[1] and other_size[0] <= my_size[0]):
+            #print('Collide !!!')
+            return True
+        else:
+            return False
+
 
     #update the new location and blit it on screen
     def update(self):
