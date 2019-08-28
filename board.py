c49a8d96a2018141dab50b3e70584c773472e6d8
direin13 <45359779+direin13@users.noreply.github.com>
2019-08-28 16:04:52 +0100

    Add files via upload

--- board.py
+++ board.py
@@ -8,11 +8,20 @@ class Board_piece(object):
       self.x = x
       self.y = y
 
-   def make_board(self):
+   def make_board(self, insert_text):
       #--multiplis the board piece accordingly--
-      for i in range(0, 560, 80):
-         for k in range(0, 480, 80):
-            win.blit(im, (self.x + i, self.y + k))
+      for x in range(0, 560, 80):
+         for y in range(0, 480, 80):
+            win.blit(im, (self.x + x, self.y + y))
+
+      #--box at the bottom pf the screen--
+      win.blit(pygame.transform.scale(pygame.image.load('player.png'), (560, 40)), (0, 560))
+
+      #--The text in the box--
+      font = pygame.font.Font(None, 36)
+      text = font.render(insert_text, 1, (255, 0, 0))
+      text_position = text.get_rect()
+      win.blit(text, (225, 570))
 
 #-----------------------------------------------------------------
 
@@ -33,19 +42,24 @@ class Chip(object):
 
 #---------------------------------------------------------------------------------
 
+pygame.init()
+
 win = pygame.display.set_mode((560, 600))
+
+font = pygame.font.Font(None, 36)
+text = font.render("Connect 4", 1, (255, 0, 10))
+text_position = text.get_rect()
 
 #--The board--
 im = pygame.transform.scale(pygame.image.load('block.png'), (80, 80))
 board = Board_piece(im, 0, 80)
-#--For now I've only got a png 'block' with a transparent circle in the middle. The circle and block measurments are not exact, but later we'll get exact measurments--
-
-
-game_on = True
+#--Every square which makes up the board is 80 x 80 units--
 
 #--The velocity of the chip--
 chip = Chip(50, 50, 25, 1)
 #-------
+
+game_on = True
 
 while game_on:
    win.fill((0, 0, 0))
@@ -55,11 +69,10 @@ while game_on:
       if event.type == pygame.QUIT:
          game_on = False
 
-   #--Everythime the loop comes around,the circle is placed and the transparent image is put on top--
+   #--Everythime the loop comes around,the circle is placed then the board is put on top--
    chip.move()
-   board.make_board()
+   board.make_board("insert_text")
    chip.gravity(330)
-
    pygame.display.update()
 
 #--the chip isn't too important. But I want to show that the circle can pass underneath the board--
