7056c98f4558357b27d5edb7c2c881b67589fa4e
optimid <48894093+optimid@users.noreply.github.com>
2019-09-01 19:38:42 +0100

    Update Chips_01.py

--- Chips_01.py
+++ Chips_01.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 #================================================================
 #mass import with exception handling
 try:
@@ -9,11 +8,11 @@ except ImportError as err:
     print("\nCould not load {}".format(err))
     sys.exit(2)
 
-
 #Connect Chip; still working on it
 #================================================================
 class Chip(object):
 
+    World = []
 
     def __init__(self, x, y, height, width, screen, image):
         self.image = pygame.image.load(image)
@@ -23,15 +22,16 @@ class Chip(object):
         self.height = height
         self.screen = screen
         self.times = 1
-        self.rect = self.image.get_rect()
+        self.location = ()
+        self.centre = x, y
+        self.y_limit = ()
+        self.x_limit = ()
 
-    #acc the accration due to gravity
-    #floor is where it will stop
+    #acc the accration due to gravity, floor is where it will stop.
     def gravity(self, acc, floor):
         if self.y < floor - (self.height) + (self.height / 10):
             self.y = self.y + acc * self.times
             self.times = self.times + 1
-
         else:
             self.y = floor - (self.height) + (self.height / 10)
 
@@ -46,9 +46,36 @@ class Chip(object):
         else:
             return False
 
+    #New collison dectection, only based off of rectanagles and squares for now
+    def collisions(self):
+        #retangle or square
+        #(top_left, top_right, bottom_right, bottom_left)
+        top_left = (self.x - (self.width / 2)), (self.y - (self.height / 2))
+        top_right = (self.x + (self.width / 2)), (self.y - (self.height / 2))
+        bottom_left = (self.x - (self.width / 2)), (self.y + (self.height / 2))
+        bottom_right = (self.x + (self.width / 2)), (self.y + (self.height / 2))
+        self.location = (top_left, top_right, bottom_right, bottom_left)
+        Chip.World.append(self.location)
+        self.y_limit = top_left[1], bottom_right[1]
+        self.x_limit = bottom_left[0], top_right[0]
+        #print('my_conner_points: {}\nmy coordinates: {},{}'.format(self.location, self.x, self.y))
+
+    def connection_line(self, other):
+        m = (other.y - self.y) / (other.x - self.x)
+        print('DRAW: DRAW')
+        for x in range(self.x, other.x):
+            y = ((m * x) - (m * self.x)) + self.y
+            if (self.x_limit[0] <= x and x <= self.x_limit[1]) and (self.y_limit[0] <= y and y <= self.y_limit[1]):
+                print('mymymy: ', (x, y))
+            elif (other.x_limit[0] <= x and x <= other.x_limit[1]) and (other.y_limit[0] <= y and y <= other.y_limit[1]):
+                print('ititit: ', (x, y))
+            else:
+                print('void_void: ', (x, y))
+
 
     #update the new location and blit it on screen
     def update(self):
+        Chip.World.remove(self.location)
         return self.screen.blit(pygame.transform.scale(self.image, (self.width, self.height)), (self.x, self.y))
 
 #================================================================
