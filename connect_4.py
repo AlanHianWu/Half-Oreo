7a0df5ba8d82c4472f7bc314ccff6edc013bcdd4
direin13 <45359779+direin13@users.noreply.github.com>
2019-09-20 16:50:28 +0100

    Add files via upload

--- connect_4.py
+++ connect_4.py
@@ -45,7 +45,7 @@ cutscene = 0
 #------------------------------------------------------------------------------------------------------------------------------------------------
 
 #---------------------------------------------------------------------- INTRO SEQUENCE ----------------------------------------------------------
-intro_songs.play_song('audio/intro_part_1.mp3')
+intro_songs.play_song('audio/intro_part_1.mp3', -1)
 
 flag = False
 while cutscene < 3 and menu == True:
@@ -53,32 +53,32 @@ while cutscene < 3 and menu == True:
    screen.fill((0, 0, 0))
 
    if cutscene < 1:
-      i_1 = fade_in((0, 0, 0), screen, 560, 600, 5, [('text', (font_1, 'POWERED', screen, (255, 255, 255), (200, 200))), 
-                                                   ('text', (font_1, 'BY', screen, (255, 255, 255), (250, 260))),
-                                                   ('text', (font_1, 'PYGAME', screen, (255, 255, 255), (205, 320)))])
+      fade_in((0, 0, 0), screen, 560, 600, 5, [('text', (font_1, 'POWERED', screen, (255, 255, 255), (200, 200))), 
+                                               ('text', (font_1, 'BY', screen, (255, 255, 255), (250, 260))),
+                                               ('text', (font_1, 'PYGAME', screen, (255, 255, 255), (205, 320)))])
 
-      i = fade_out((0, 0, 0), screen, 560, 600, 5, [('text', (font_1, 'POWERED', screen, (255, 255, 255), (200, 200))), 
-                                                    ('text', (font_1, 'BY', screen, (255, 255, 255), (250, 260))),
-                                                    ('text', (font_1, 'PYGAME', screen, (255, 255, 255), (205, 320)))])
+      fade_out((0, 0, 0), screen, 560, 600, 5, [('text', (font_1, 'POWERED', screen, (255, 255, 255), (200, 200))), 
+                                                ('text', (font_1, 'BY', screen, (255, 255, 255), (250, 260))),
+                                                ('text', (font_1, 'PYGAME', screen, (255, 255, 255), (205, 320)))])
       cutscene = 1
 
    if cutscene < 2:
-      i_2 = fade_in((0, 0, 0), screen, 560, 600, 5, [('image', (all_images['half_oreo'], screen, (110, 60), (330, 350))),
-                                                   ('text', (font_1, 'Production', screen, (255, 255, 255), (190, 400)))])
+      fade_in((0, 0, 0), screen, 560, 600, 5, [('image', (all_images['half_oreo'], screen, (110, 60), (330, 350))),
+                                               ('text', (font_1, 'Production', screen, (255, 255, 255), (190, 400)))])
 
-      i_3 = fade_out((0, 0, 0), screen, 560, 600, 5, [('image', (all_images['half_oreo'], screen, (110, 60), (330, 350))),
-                                                    ('text', (font_1, 'Production', screen, (255, 255, 255), (190, 400)))])
+      fade_out((0, 0, 0), screen, 560, 600, 5, [('image', (all_images['half_oreo'], screen, (110, 60), (330, 350))),
+                                                ('text', (font_1, 'Production', screen, (255, 255, 255), (190, 400)))])
       cutscene = 2
 
    if cutscene < 3:
-      i_4 = fade_out((255, 255, 255), screen, 560, 600, 8)
+      fade_out((255, 255, 255), screen, 560, 600, 8)
       break
 
    pygame.display.update()
 
 #--Did The play skip any fade ins?--
-if i > 0 or i_2 > 0 or i_3 > 0 or i_4 > 0:
-   intro_songs.play_song('audio/intro_part_2.mp3')
+
+intro_songs.play_song('audio/intro_part_2.mp3', -1)
 #-------------------------------------------------------------------
 
 #-------------------------------------------------------------------
@@ -87,7 +87,6 @@ if i > 0 or i_2 > 0 or i_3 > 0 or i_4 > 0:
 
 while game_on == True and menu == True:
    screen.fill((0, 0, 0))
-   keys = pygame.key.get_pressed()
    mouse_x, mouse_y = pygame.mouse.get_pos()
    local_button.cycle()
    Multiplayer_button.cycle()
@@ -114,6 +113,8 @@ while game_on == True and menu == True:
          intro_songs.stop()
          fade_out((255, 255, 255), screen, 560, 600, 3)
          menu = False
+      if event.type == pygame.MOUSEBUTTONDOWN and Multiplayer_button.collision(mouse_x, mouse_y) == True:
+         play_sound('select', all_sounds, 0)
 
    pygame.time.delay(30)
    pygame.display.update()
@@ -134,7 +135,7 @@ floor = Sprite_object_basic('floor', 0, 560, 560, 40)
 #--------
 
 #--The first chip--
-chip = Chip(0, 1, (255, 255, 0), 0, 0, 80, 80, 1, all_images['chip'], False)
+chip = Chip(0, 1, (255, 255, 0), 0, 0, 80, 80, 1, all_images['chip_red'], False)
 #-------
 
 #--Music--
@@ -163,13 +164,13 @@ while game_on:
    if chip.is_falling is True:
       fall = chip.gravity(floor)
       if fall is 1 and current_player is 1:
-         chip = Chip(i, current_player + 1, (255, 0, 0), mx, 0, 80, 80, 1, image=all_images['chip'], is_falling=False)
+         chip = Chip(i, current_player + 1, (255, 0, 0), mx, 0, 80, 80, 1, image=all_images['chip_yellow'], is_falling=False)
          chip.move(mx)
          i += 1
          current_player = 2
 
       elif fall is 1 and current_player is 2:
-         chip = Chip(i, current_player - 1, (255, 255, 0), mx, 0, 80, 80, 1, image=all_images['chip'], is_falling=False)
+         chip = Chip(i, current_player - 1, (255, 255, 0), mx, 0, 80, 80, 1, image=all_images['chip_red'], is_falling=False)
          chip.move(mx)
          i += 1
          current_player = 1
